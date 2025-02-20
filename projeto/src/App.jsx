cad9eb41b19faf0a8048ead907ccec88f1f82cd9
Felipe Cagnin <cagnin.lima.2007@gmail.com>
2025-02-20 11:59:26 -0300

    VERSIONDEFINITIVA

--- projeto/src/App.jsx
+++ projeto/src/App.jsx
@@ -1,22 +1,20 @@
 import "./App.css";
 import Header from "./components/Header";
 import Pe from "./components/Footer";
-import ListaTarefas from "./components/ListaTarefas.jsx"
+import ListaTarefas from "./components/ListaTarefas.jsx";
 
 function App() {
   return (
     <>
-      <body>
-        <header>
-          <Header />
-        </header>
-        <section>
-          <ListaTarefas/>
-        </section>
-        <footer>
-          <Pe />
-        </footer>
-      </body>
+      <header>
+        <Header />
+      </header>
+      <section>
+        <ListaTarefas />
+      </section>
+      <footer>
+        <Pe />
+      </footer>
     </>
   );
 }
