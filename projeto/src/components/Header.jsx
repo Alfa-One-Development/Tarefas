d9c3b756706157a306bc16406d04266e07b521b6
Murilo Correia <mucorreia008@gmail.com>
2025-02-20 08:41:44 -0300

    heder

---
+++ projeto/src/components/Header.jsx
@@ -0,0 +1,12 @@
+//header component jsx
+import './styles/Header.css'
+
+function Cabeca(){
+    return(
+        <div className="cabeca">
+            <h1>Lista de Tarefas ✔</h1>
+        </div>
+    )
+}
+
+export default Cabeca;
