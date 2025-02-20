7824b84c2e60de1abcd8306df12aed96dc0a8551
Vitor Izidoro <vitorizidoro288@gmail.com>
2025-02-20 08:42:18 -0300

    V1

---
+++ projeto/src/components/ListaTarefas.jsx
@@ -0,0 +1,12 @@
+import Tarefas from './Tarefas';
+import './styles/ListaTarefas.css'
+function ListaTarefas(){
+    return(
+        <>
+        <div>
+            <Tarefas/>
+        </div>
+        </>
+    )
+}
+export default ListaTarefas
