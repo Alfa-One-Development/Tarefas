da2973fa725518f6dc9ba98574be4d422347a4d7
Vitor Izidoro <vitorizidoro288@gmail.com>
2025-02-20 08:56:17 -0300

    V2

--- projeto/src/components/ListaTarefas.jsx
+++ projeto/src/components/ListaTarefas.jsx
@@ -3,8 +3,26 @@ import './styles/ListaTarefas.css'
 function ListaTarefas(){
     return(
         <>
-        <div>
-            <Tarefas/>
+        <div className='Lista'>
+            <Tarefas
+            tarefa = "Jogar 'The President-Brazil Edition'"
+            />
+            <Tarefas
+            tarefa = "Estudar Java Script"
+            />
+            <Tarefas
+            tarefa = "Fazer um site sobre React"
+            />
+            <Tarefas
+            tarefa = "Escrever um texto sobre React" 
+            />
+            <Tarefas
+            tarefa = "William zerar o jogo 'The President-Brazil Edition'"
+            />
+            <Tarefas
+            tarefa = "Fazer a atividade 'Lista De Tarefas'"
+            />
+
         </div>
         </>
     )
